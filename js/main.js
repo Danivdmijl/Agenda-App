af828b153159d1e2a975b8ad8ebf58c8addb37c2
Danivdmijl <32570@ma-web.nl>
2023-05-10 13:10:58 +0200

    ja

---
+++ js/main.js
@@ -0,0 +1,66 @@
+class AgendaApp {
+    api;
+    agenda;
+
+    constructor() {
+        this.api = new API();
+        let result = this.api.getData().then(result => {
+        })
+        this.agenda = new Agenda();
+    }
+}
+
+class API {
+    dataFromAPI = [];
+
+  async  getData() {
+       await fetch("../data/data.json").then(response => {
+            return response.json();
+        }).then(data => {
+            this.dataFromAPI = data.months;
+        })
+        return this.dataFromAPI;
+    }
+}
+
+class Agenda {
+    renderer;
+    header;
+    month;
+    constructor() {
+        this.renderer = new Renderer();
+        this.header = new Header();
+        this.month = new Month(this);
+    }
+}
+
+class Renderer {
+
+}
+
+class Header {
+
+}
+
+class Month {
+    agenda;
+    days = [];
+
+    constructor(agenda) {
+        for (let i = 0; i < 31; i++) {
+            this.days.push(new Day(this));
+        }
+        this.agenda = agenda;
+    }
+}
+
+class Day {
+    month;
+
+    constructor(month) {
+        this.month = month;
+    }
+}
+
+const DaniAgenda = new AgendaApp();
+
